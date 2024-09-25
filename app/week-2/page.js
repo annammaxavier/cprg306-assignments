d6ec0091540e2454560f64451fca4624b4a6d341
annammaxavier <166633700+annammaxavier@users.noreply.github.com>
2024-09-24 22:00:04 -0600

    app/page.js file
    Add code in app/page.js file

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import StudentInfo from "C:/cprg306/cprg306-assignments/app/week-2/studentinfo.js";
+
+
 export default function Page() {
     return (
       <div>
---
+++ app/week-2/studentinfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+
+const StudentInfo = () => {
+  return (
+    <div>
+    <p>AnnammaXavier</p>
+    <Link href="https://github.com/annammaxavier">https://github.com/annammaxavier</Link>
+     
+   
+   
+</div>
+
+      
+   
+  );
+};
+
+export default StudentInfo;
