2c802f9e0737863b17dc4ed3f9f073047c204c2f
annammaxavier <166633700+annammaxavier@users.noreply.github.com>
2024-10-21 11:25:30 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import ItemList from "./week-6/item-list";
 import NewItem from "./week-6/item-list";
 export default function Page() {
   return (
-    <main> 
+    
+       <main>
+       <h1>Shopping List</h1>
       <ItemList />
     </main>
   );
